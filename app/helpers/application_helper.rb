dd552a5be877274338744f9483adbf5c0fa6d672
teruyotasaki <teruyotasaki@gmail.com>
2016-08-24 12:00:58 +0000

    view complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,24 +11,36 @@ module ApplicationHelper
   end
 
   def csv_split(file)
-    newarray = Array.new
 
+  begin
+    headers, *lines = CSV.read(file.path,  {encoding: "Shift_JIS:UTF-8", undef: :replace, replace: "*",:row_sep => :auto})
+    return headers,lines
+  rescue => e
+    headers, *lines = CSV.read(file.path,  {encoding: "Shift_JIS:UTF-8", undef: :replace, replace: "*",:row_sep => "\r\n"})
+    return headers,lines
+  end
+
+#    newarray = Array.new
     # Shift-JISなCSVを読み込む・書き出しするときにエラーを起こさない数少ない方法
     # http://qiita.com/yuuna/items/7e4e06a1b881d85697e9
-    CSV.foreach(file.path,  {encoding: "Shift_JIS:UTF-8", undef: :replace, replace: "*", headers: true}) do |row|
-      newarray.push(row)
-
-
-
-  
-#      # IDが見つかれば、レコードを呼び出し、見つかれなければ、新しく作成
-#      product = find_by(id: row["id"]) || new
-#      # CSVからデータを取得し、設定する
-#      product.attributes = row.to_hash.slice(*updatable_attributes)
-#      # 保存する
-#      product.save!
-    end
-
-    return newarray
+#    CSV.foreach(file.path,  {encoding: "Shift_JIS:UTF-8", undef: :replace, replace: "*", headers: true}) do |row|
+#      #puts row.length
+##      newarray.push(row)
+##      line = row.split(",")
+#      row.each do |key,value|
+#        if key == "Placement Tag"
+##puts value
+##          value = value.gsub(/^\"/, "")
+##          value = value.gsub(/\"\"/,"\"")
+##          value = value.gsub(/\"$/,"")
+#        end
+#        row[key] = value
+##        #newarray.push(row)
+#      end
+#      newarray.push(row)
+#
+#    end
+#
+#    return newarray
   end
 end
